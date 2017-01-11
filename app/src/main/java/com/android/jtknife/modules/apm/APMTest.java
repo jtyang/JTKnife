1afc3a4a7ea873d80897f0829d9de862ed340b39
yangjiantong <yangjiantong1988@126.com>
2017-01-11 19:30:56 +0800

    add newrelic apm

---
+++ app/src/main/java/com/android/jtknife/modules/apm/APMTest.java
@@ -0,0 +1,23 @@
+package com.android.jtknife.modules.apm;
+
+import android.content.Context;
+
+import com.newrelic.agent.android.NewRelic;
+import com.newrelic.agent.android.logging.AgentLog;
+
+/**
+ * 国外newrelic apm检测sdk使用
+ * AUTHOR: yangjiantong
+ * DATE: 2017/1/11
+ * <p>
+ * https://docs.newrelic.com/docs/mobile-monitoring/new-relic-mobile-android/install-configure/installing-android-apps-gradle-android-studio
+ * https://docs.newrelic.com/docs/mobile-monitoring/new-relic-mobile-android/install-configure/work-android-sdk-api
+ */
+public class APMTest {
+
+    public void init(Context context) {
+        NewRelic.withApplicationToken("AAa501598268399fb6c02675e9493db0d328bf7f70")
+                .withLogLevel(AgentLog.DEBUG)
+                .start(context.getApplicationContext());
+    }
+}
