18ac58dba5d5923aa3e34404b0793695e4808cca
yangjiantong <yangjiantong1988@126.com>
2017-12-20 15:05:51 +0800

    add basedelegate with lifecycle observer

---
+++ jtknifecore/src/main/java/com/android/jtknife/core/app/BaseDelegate.java
@@ -0,0 +1,49 @@
+package com.android.jtknife.core.app;
+
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.OnLifecycleEvent;
+import android.content.Context;
+import android.view.View;
+
+/**
+ * 文件描述:
+ *
+ * UseAge:
+ *
+ * public class TestDelegate extends BaseDelegate{}
+ *
+ * XxActivity:
+ * onCreate
+ * TestDelegate testDelegate = new TestDelegate();
+ * getLifecycle().addObserver(testDelegate);
+ *
+ * onDestroy:
+ * getLifecycle().removeObserver(testDelegate);
+ *
+ * @author yangjiantong
+ * @date 2017/12/20
+ */
+public abstract class BaseDelegate implements LifecycleObserver {
+
+    private Context mContext;
+    private View mRootView;
+
+    public BaseDelegate(Context context, View rootView) {
+        this.mContext = context;
+        this.mRootView = rootView;
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    public void onCreate() {
+
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    public void onDestroy() {
+        //疑问：Activity的onDestroy应该何时执行getLifecycle().removeObserver(testDelegate)比较合适？
+        //或者说如何能保证Delegate能够一定执行到onDestroy并且又能够removeObserver(delegate)
+    }
+
+
+}
